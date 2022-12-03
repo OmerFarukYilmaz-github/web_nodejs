f143b090a29c56943002a56fdf8273938a996d64
OmerFarukYilmaz-github <66321088+OmerFarukYilmaz-github@users.noreply.github.com>
2022-12-03 20:54:20 +0300

    feat: add login and forgot password routes
    In the login routes, a json web token is generated
    In the forgot password route admin sends you an email which contain mail and pasword  infos.

--- Web Projesi/routes/user.js
+++ Web Projesi/routes/user.js
@@ -3,6 +3,12 @@ const { query } = require('../connection');
 const connection = require('../connection');
 const router = express.Router();
 
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+
+
 router.post('/signup',  (req,res) =>  
 {
     let user = req.body;
@@ -40,5 +46,98 @@ router.post('/signup',  (req,res) =>
     })
 })
 
+router.post('/login', (req,res)=>
+{
+    const user = req.body;
+    var query = "select email, password, role, status from tbl_user where email=?"
+    connection.query(query, [user.email], (err,results)=>
+    {
+        if(err)
+        {
+            return res.status(500).json(err);
+        }
+        else
+        {
+            if(results.length <= 0 || results[0].password != user.password)
+            {
+                return res.status(401).json({message: "Wrong UserName or password"});
+            }
+            else if(results[0].status == "false")
+            {
+                return res.status(401).json({message: "Please wait for Admin approval"});
+            }
+            else if (results[0].password == user.password)
+            {
+                const response = {email: results[0].email, role: results[0].role};
+                const accessToken = jwt.sign(response, process.env.TOKEN_KEY, {expiresIn:"8H"});
+                res.status(200).json({token: accessToken}); 
+
+            }
+            else
+            {
+                return res.status(400).json({message: "Unfortunately a mistake occured!!"});
+            }
+        }
+    })
+})
+
+var transporter = nodemailer.createTransport
+(
+    {
+        service: "gmail",
+        auth: 
+        {
+            user: process.env.EMAIL,
+            pass: process.env.EMAIL_PASSWORD,
+        }
+    }
+)
+
+router.post('/forgetPassword', (req, res)=>
+{
+    const user = req.body;
+    var query = "select email, password from tbl_user where email=?"
+    connection.query(query, [user.email], (err,results)=>
+    {
+        if(err)
+        {
+            return res.status(500).json(err);
+        }
+        else
+        {
+            if(results.length <=0)
+            {
+                return res.status(200).json({message: "There is no user with this e-mail"});
+            }
+            else
+            {
+                var mailOptions = 
+                {
+                    from: process.env.EMAIL,
+                    to: results[0].email,
+                    subject: 'OFY-The Admin',
+                    html: "<p><b>Your Infos for login   </b><br><b>Email:  </b>"+results[0].email+
+                          "<br><b>Password: </b>"+results[0].password+
+                          "<br><a href='http://localhost:4200/'>Login Page</a;></p>"
+                };
+
+                transporter.sendMail(mailOptions, function(err, info)
+                {
+                    if(err)
+                    {
+                        console.log(err);
+                    }
+                    else
+                    {
+                        console.log("Forget Password mail has been sent:    "+info.response );
+                    }
+                });
+
+                return res.status(200).json({message: "Login infos sent to your mail"});
+            }
+        }
+    })
+
+})
 
 module.exports= router;
