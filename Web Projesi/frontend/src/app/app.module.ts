5f0691286b76b714ff2e118d7f742c1b5ef5011e
OmerFarukYilmaz-github <66321088+OmerFarukYilmaz-github@users.noreply.github.com>
2022-12-15 20:50:32 +0300

    feat: add signup page and connect to the backend

--- Web Projesi/frontend/src/app/app.module.ts
+++ Web Projesi/frontend/src/app/app.module.ts
@@ -12,13 +12,33 @@ import { SharedModule } from './shared/shared.module';
 import { FullComponent } from './layouts/full/full.component';
 import { AppHeaderComponent } from './layouts/full/header/header.component';
 import { AppSidebarComponent } from './layouts/full/sidebar/sidebar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SignupComponent } from './signup/signup.component';
+import  {NgxUiLoaderModule, NgxUiLoaderConfig, SPINNER, PB_DIRECTION} from "ngx-ui-loader";
+
+
+const ngxUiLoaderConfig: NgxUiLoaderConfig={
+  text:"Loading...",
+  textColor:"#FFFFFF",
+  textPosition:'center-center',
+  pbColor:"red",
+  bgsColor:"red",
+  fgsColor:"red",
+  fgsType:SPINNER.fadingCircle,
+  fgsSize:100,
+  pbDirection:PB_DIRECTION.leftToRight,
+  pbThickness:5
+}
+
+
 @NgModule({
   declarations: [	
     AppComponent,
     HomeComponent,
     FullComponent,
     AppHeaderComponent,
-    AppSidebarComponent
+    AppSidebarComponent,
+    SignupComponent
    ],
   imports: [
     BrowserModule,
@@ -28,7 +48,9 @@ import { AppSidebarComponent } from './layouts/full/sidebar/sidebar.component';
     ReactiveFormsModule,
     MaterialModule,
     FlexLayoutModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule,
+    NgxUiLoaderModule.forRoot(ngxUiLoaderConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Web Projesi/frontend/src/app/shared/global-constant.ts
@@ -0,0 +1,11 @@
+export class GlobalConstant{
+    public static genericError:string = "Something went wrong!";
+
+    // değerler doğru formatta olsun
+    public static nameRegex: string = "[a-zA-Z0-9 ]*";
+    public static mailRegex: string = "[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}";
+    public static contactNumberRegex: string = "^[e0-9]{10,10}$";
+
+    public static error:string="error";
+
+}
